2cb039f19ec4af312618688ea2961cc80d017b5f
Ahmed Demirezen <47192102+feezx1@users.noreply.github.com>
2019-02-03 13:21:25 +0300

    Add files via upload

---
+++ ascii_art.py
@@ -0,0 +1,52 @@
+#
+#
+      #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ### 
+     # # # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   # 
+     ### ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   ## 
+     # # # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #       
+     # # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  #  
+#
+#
+#
+#
+#
+#
+#
+#
+
+
+#Ahmed Demirezen Feezx1
+
+import sys
+import math
+
+l = 4
+h = 5
+t = input("Lutfen cikti alinacak string i giriniz:\n")
+l_ilk=l
+harf_list=[" #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ###","# # # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   #","### ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   ##","# # # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #      ","# # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  # "]
+harfler="ABCDEFGHIJKLMNOPQRSTUVWXYZ?"
+sozluk={}
+test=[]
+for i in range(len(harfler)):
+    sozluk[harfler[i]]=(l-l_ilk,l)
+    l=l+l_ilk
+  
+    
+
+
+t=t.upper()
+j=0
+satir_s=len(t)
+
+
+    
+for k in range(h):
+    for m in range(len(t)):
+        if harfler.count(t[m])==1:
+            print(harf_list[k][sozluk[t[m]][0]:sozluk[t[m]][1]],end="")
+        else:
+            print(harf_list[k][sozluk["?"][0]:sozluk["?"][1]],end="")
+            
+    print("")
+                
